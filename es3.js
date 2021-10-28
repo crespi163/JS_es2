9daf9eab2f074e46a04efb0946614ab8c9153028
Crespi Francesco <crespi.francesco@osdbsesto>
2021-10-28 11:40:30 +0200

    gggg

---
+++ es3.js
@@ -0,0 +1,16 @@
+$("#btn+").click(function () {
+    var input1=$("num1");
+    var input2=$("num2");
+    console.log("input1", input1.val());
+    console.log("input2", input1.val());
+    if(input.val() && input2.val()){
+    var n1=parseInt(input1.val());
+    var n1=parseInt(input2.val());
+    var risultato=n1+n2;
+    var String=
+    "<tr><td>"+N1+"<td/><td style='yext-align:center'>"+"+"+"<td/><td>"+N2+"<td/><td>"+result+"<td/></tr>";
+    $("#tab").appened(string)
+    input1.val("");
+    input2.val("");
+    }
+});
